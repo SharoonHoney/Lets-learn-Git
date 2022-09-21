a6a8f9d09f867070de5ecf1bc930b577b36a8004
Sharoon Honey <sw.sharoonhoney@gmail.com>
2022-09-22 00:59:13 +0500

    adding diff.java

--- diff.java
+++ diff.java
@@ -1,7 +1,7 @@
 public class Sum{
     public static void main(String[] args){
-        int a = 2;
-        int b = 6;
+        int a = 7;
+        int b = 5;
         //Print Output
         System.out.println("The Difference is "+(a-b));
     }
